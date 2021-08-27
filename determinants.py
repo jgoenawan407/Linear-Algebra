f3bb646aa52f7d65307c6edae7fdfcccdfea1266
jgoenawan407 <68802410+jgoenawan407@users.noreply.github.com>
2021-08-26 18:56:05 -0700

    Add files via upload

---
+++ determinants.py
@@ -0,0 +1,63 @@
+# Jackson Goenawan, 8/26/21
+
+import copy
+
+def det(m): # recursively runs determinant process until you get to a 2x2
+
+  if len(m) != len(m[0]):
+    return("Matrix is not square")
+
+  if len(m) == 2:
+    return (m[0][0] * m[1][1]) - (m[0][1] * m[1][0])
+
+  det_sum = 0
+
+  for i in range(len(m)):
+    
+    # matrix excluding the row and column we're currently "covering"  
+    m_p = [row[: i] + row[i + 1:] for row in (m[: 0] + m[1:])] # must include m[: 0] term or else it just doesn't count anything for the sub matrix
+    sign = (-1) ** i # accounts for sign pattern
+    det_sum += sign * m[0][i] * det(m_p)
+
+  return det_sum
+
+def cramer_solve(coeff, constants):
+	
+	d = det(coeff) # determinant of coefficient matrix
+
+	if d == 0:
+		return("System is linearly independent")
+
+	solns = [[0] for i in range(len(coeff))]
+
+	for i in range(len(coeff)):
+
+		m_i = copy.deepcopy(coeff)
+
+		for j in range(len(coeff[0])):
+
+			m_i[j][i] = constants[j][0]
+
+		solns[i] = det(m_i) / d
+
+	return(solns)
+
+m1 = [[1, 2, 3], [4, 5, 6]] # returns error
+m2 = [[0, 4, -1, 5], [-4, 0, 3, 2], [1, -3, 0, 1], [-5, 2, -1, 0]]
+m3 = [[4, 7, 0, 0], [2, 8, 0, 0], [0, 0, 1, 5],[0, 0, -2, 2]]
+diag = [[4, -1, 8], [0, 2, 3], [0, 0, 5]]
+
+print('Matrix 1: ' + str(m1))
+print('Matrix 2: ' + str(m2))
+print('Matrix 3: ' + str(m3))
+print('Diagonal Matrix: ' + str(diag))
+
+print('Determinant of Matrix 1: ' + str(det(m1)))
+print('Determinant of Matrix 1: ' + str(det(m2)))
+print('Determinant of Matrix 1: ' + str(det(m3)))
+print('Determinant of Matrix 1: ' + str(det(diag)))
+
+# solving system with cramer's rule
+coeff = [[3, -5], [6, 16]] # matrix holding variable coefficients
+constants = [[15.5], [5.0]] # constants (on right side of =)
+print('Cramer Solution: ' + str(cramer_solve(coeff, constants)))
---
+++ matrix_arithmetic.py
@@ -0,0 +1,65 @@
+# Jackson Goenawan, 8/23/21
+
+import copy
+
+def scale(scalar, matrix):
+  
+  scaled = copy.deepcopy(matrix)
+
+  for r in range(len(matrix)): 
+    #r starts at 0 and goes until end of matrix (exclusive of last index)
+    for c in range(len(matrix[0])):
+      scaled[r][c] = scalar * matrix[r][c]
+  return(scaled)
+    
+def add(m1, m2):
+  
+  r = len(m1)
+  c = len(m1[0])
+  
+  if len(m1) != len(m2) or len(m1[0]) != len(m2[0]):
+    return("Matrix dimensions do not agree")
+    
+  else:
+    M3 = copy.deepcopy(m1) # alr checked that this has same dimensions as m2
+    for i in range(r):
+      for j in range(c):
+        M3[i][j] = m1[i][j] + m2[i][j]
+    return(M3)    
+
+def mult(m1, m2):
+
+  if len(m1[0]) != len(m2):
+    return("Matrix dimensions do not agree")
+
+  else:
+
+    r = len(m1)
+    c = len(m2[0])
+    m = [([0] * len(m2[0])) for i in range(len(m1))]
+    
+    for i in range(len(m)):
+      for j in range(len(m[0])):
+        
+        numTerms = len(m1[0]) # how many products we need to sum
+        for k in range(numTerms):
+          m[i][j] += m1[i][k] * m2[k][j] # moving horizontally through m1, moving vertically through m2
+
+    return(m)
+
+m1 = [[1, 2, 3], [4, 5, 6]]
+m2 = [[4, 6, 7], [9, 11, 2]]
+m3 = [[4, 7], [10, 3], [8, 2]] # can multiply m1m3 or m2m3
+# m4 = input("enter matrix for determinant calculation"), tough to do matrix input, since we'd have to split by element and then cast each element to int
+
+print('Matrix 1: ' + str(m1))
+print('Matrix 2: ' + str(m2))
+print('Matrix 3: ' + str(m3))
+
+print('Scaled matrix 1 by 6: ' + str(scale(6, m1)))
+
+print('Adding matrices 1 and 2: ' + str(add(m1, m2)))
+print('Adding matrices 1 and 3: ' + str(add(m1, m3))) # should return error
+
+print('Multiplying matrices 2 and 3: ' + str(mult(m2, m3)))
+print('Multiplying matrices 1 and 2: ' + str(mult(m1, m2))) # should return error
